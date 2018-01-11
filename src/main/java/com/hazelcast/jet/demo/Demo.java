e9d1c8619a1e228f892f832f8a8bbfa1fe1251f6
Emin Demirci <emindemirci@gmail.com>
2018-01-11 18:12:03 +0300

    wip

--- src/main/java/com/hazelcast/jet/demo/Demo.java
+++ src/main/java/com/hazelcast/jet/demo/Demo.java
@@ -3,9 +3,7 @@ package com.hazelcast.jet.demo;
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.jet.Job;
-import com.hazelcast.jet.accumulator.LinTrendAccumulator;
 import com.hazelcast.jet.aggregate.AggregateOperation1;
-import com.hazelcast.jet.aggregate.AggregateOperations;
 import com.hazelcast.jet.core.DAG;
 import com.hazelcast.jet.core.TimestampKind;
 import com.hazelcast.jet.core.Vertex;
@@ -14,19 +12,67 @@ import com.hazelcast.jet.core.processor.DiagnosticProcessors;
 import com.hazelcast.jet.core.processor.Processors;
 import com.hazelcast.jet.datamodel.TimestampedEntry;
 import com.hazelcast.jet.datamodel.Tuple2;
+import com.hazelcast.jet.datamodel.Tuple3;
+import com.hazelcast.jet.demo.types.WakeTurbulanceCategory;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import static com.hazelcast.jet.aggregate.AggregateOperations.allOf;
+import static com.hazelcast.jet.aggregate.AggregateOperations.averagingLong;
+import static com.hazelcast.jet.aggregate.AggregateOperations.linearTrend;
+import static com.hazelcast.jet.aggregate.AggregateOperations.toList;
 import static com.hazelcast.jet.core.Edge.between;
 import static com.hazelcast.jet.core.Edge.from;
 import static com.hazelcast.jet.core.WatermarkEmissionPolicy.emitByFrame;
-import static com.hazelcast.jet.core.WatermarkPolicies.withFixedLag;
+import static com.hazelcast.jet.core.WatermarkPolicies.limitingLagAndDelay;
 import static com.hazelcast.jet.core.processor.Processors.accumulateByFrameP;
+import static com.hazelcast.jet.core.processor.Processors.aggregateToSlidingWindowP;
 import static com.hazelcast.jet.core.processor.Processors.combineToSlidingWindowP;
 import static com.hazelcast.jet.core.processor.Processors.insertWatermarksP;
+import static com.hazelcast.jet.demo.types.WakeTurbulanceCategory.HEAVY;
 import static com.hazelcast.jet.demo.util.Util.inLondon;
+import static com.hazelcast.jet.function.DistributedFunctions.constantKey;
 
 public class Demo {
+
+    static Map<String, Double> typeToLTOCycyleC02Emission = new HashMap<String, Double>() {{
+        put("B738", 2625d);
+        put("A320", 2750.7d);
+        put("A321", 2560d);
+        put("A319", 2169.8d);
+        put("B763", 5405d);
+        put("E190", 1605d);
+        put("B773", 7588d);
+        put("A333", 5934d);
+        put("B752", 4292d);
+        put("A332", 7029d);
+        put("B737", 2454d);
+        put("DH8D", 1950d);
+        put("B772", 7346d);
+        put("B788", 10944d);
+        put("B739", 2500d);
+        put("A388", 13048d);
+        put("B744", 10456d);
+        put("B789", 10300d);
+        put("E170", 1516d);
+        put("A306", 5200d);
+        put("B77L", 9076d);
+        put("MD11", 8277d);
+        put("E145", 1505d);
+        put("B77W", 9736d);
+        put("A318", 2169d);
+        put("B748", 10400d);
+        put("B735", 2305d);
+        put("A359", 6026d);
+        put("CRJ9", 2754d);
+        put("CRJ2", 1560d);
+        put("CRJ7", 2010d);
+        put("DC10", 7460d);
+    }};
 
     static SortedMap<Integer, Integer> heavyWTCClimbingAltitudeToNoiseDb = new TreeMap<Integer, Integer>() {{
         put(500, 96);
@@ -66,13 +112,13 @@ public class Demo {
         Vertex overLondon = dag.newVertex("planes over london", Processors.filterP((Aircraft ac) -> ac.getAlt() > 0 && inLondon(ac.getLon(), ac.getLat()) && !ac.isGnd()));
         Vertex lessThan3000 = dag.newVertex("altitude less than 3000", Processors.filterP((Aircraft ac) -> ac.getAlt() > 0 && ac.getAlt() < 3000 && !ac.isGnd()));
 
-        WindowDefinition wDefTrend = WindowDefinition.slidingWindowDef(10_000, 1_000);
-        AggregateOperation1<Aircraft, LinTrendAccumulator, Double> aggrOpTrend =
-                AggregateOperations.linearTrend(Aircraft::getPosTime, Aircraft::getAlt);
+        WindowDefinition wDefTrend = WindowDefinition.slidingWindowDef(10_000, 5_000);
+        AggregateOperation1<Aircraft, List<Object>, List<Object>> allOf = allOf(toList(),
+                linearTrend(Aircraft::getPosTime, Aircraft::getAlt));
 
 
         Vertex insertWm = dag.newVertex("insertWm",
-                insertWatermarksP(Aircraft::getPosTime, withFixedLag(25000), emitByFrame(wDefTrend)));
+                insertWatermarksP(Aircraft::getPosTime, limitingLagAndDelay(60000, 5000), emitByFrame(wDefTrend)));
 
         // accumulation: calculate altitude trend
         Vertex trendStage1 = dag.newVertex("trendStage1",
@@ -80,58 +126,94 @@ public class Demo {
                         Aircraft::getId,
                         Aircraft::getPosTime, TimestampKind.EVENT,
                         wDefTrend,
-                        aggrOpTrend));
-        Vertex trendStage2 = dag.newVertex("trendStage2", combineToSlidingWindowP(wDefTrend, aggrOpTrend));
+                        allOf));
+        Vertex trendStage2 = dag.newVertex("trendStage2", combineToSlidingWindowP(wDefTrend, allOf));
 
-        Vertex identifyPhase = dag.newVertex("identify flight phase", Processors.mapP((TimestampedEntry<Long, Double> entry) -> {
-                    if (entry.getValue() > 0) {
-                        return Tuple2.tuple2(entry.getKey(), "TAKING_OFF");
-                    } else if (entry.getValue() < 0) {
-                        return Tuple2.tuple2(entry.getKey(), "LANDING");
+        Vertex identifyPhase = dag.newVertex("identify flight phase", Processors.mapP((TimestampedEntry<Long, List> entry) -> {
+                    List value = entry.getValue();
+                    Aircraft aircraft = ((List<Aircraft>) value.get(0)).iterator().next();
+                    Double coefficient = (Double) value.get(1);
+                    if (coefficient > 0) {
+                        return Tuple2.tuple2(aircraft, "TAKING_OFF");
+                    } else if (coefficient < 0) {
+                        return Tuple2.tuple2(aircraft, "LANDING");
                     }
                     return null;
                 }
         ));
 
 
-        Vertex enrichWithNoiseInfo = dag.newVertex("enrich with noise info", Processors.mapP((Aircraft ac) -> {
-            Long altitude = ac.getAlt();
-            Integer currentDb;
-            switch (ac.getWtc()) {
-                case HEAVY:
-                    currentDb = heavyWTCClimbingAltitudeToNoiseDb.tailMap(altitude.intValue()).values().iterator().next();
-                    return Tuple2.tuple2(ac, currentDb);
-                case MEDIUM:
-                case LIGHT:
-                    currentDb = mediumWTCClimbingAltitudeToNoiseDb.tailMap(altitude.intValue()).values().iterator().next();
-                    return Tuple2.tuple2(ac, currentDb);
-                case NONE:
-                    break;
-            }
-            return null;
+        Vertex enrichWithNoiseInfo = dag.newVertex("enrich with noise info", Processors.mapP((Tuple2<Aircraft, String> tuple2) -> {
+            Aircraft aircraft = tuple2.f0();
+            String phase = tuple2.f1();
+            Long altitude = aircraft.getAlt();
+            Integer currentDb = getPhaseNoiseLookupTable(phase, aircraft.getWtc()).tailMap(altitude.intValue()).values().iterator().next();
+            return Tuple3.tuple3(aircraft, phase, currentDb);
 
         }));
+
+
+        Vertex averageNoise = dag.newVertex("average noise", aggregateToSlidingWindowP(
+                constantKey(),
+                (Tuple3<Aircraft, String, Integer> tuple3) -> tuple3.f0().getPosTime(),
+                TimestampKind.EVENT,
+                wDefTrend,
+                averagingLong(
+                        (Tuple3<Aircraft, String, Integer> tuple3) -> tuple3.f2()
+                )
+                )
+        );
+
+        Vertex pollution = dag.newVertex("average pollution", Processors.mapP((Tuple2<Aircraft, String> tuple2) -> {
+            Aircraft aircraft = tuple2.f0();
+            String phase = tuple2.f1();
+            Long altitude = aircraft.getAlt();
+            Integer currentDb = getPhaseNoiseLookupTable(phase, aircraft.getWtc()).tailMap(altitude.intValue()).values().iterator().next();
+            return Tuple3.tuple3(aircraft, phase, currentDb);
+        }));
+
         Vertex loggerSink = dag.newVertex("logger", DiagnosticProcessors.writeLoggerP());
         dag.edge(from(source, 0).to(overLondon, 0));
         dag.edge(from(overLondon, 0).to(lessThan3000, 0));
-        dag.edge(from(lessThan3000, 0).to(enrichWithNoiseInfo, 0));
-        dag.edge(from(enrichWithNoiseInfo, 0).to(loggerSink, 0));
-
 
         dag
-                .edge(from(lessThan3000, 1).to(insertWm, 0))
+                .edge(from(lessThan3000, 0).to(insertWm, 0))
                 .edge(between(insertWm, trendStage1)
                         .partitioned(Aircraft::getId))
                 .edge(between(trendStage1, trendStage2)
                         .partitioned((TimestampedEntry e) -> e.getKey()).distributed())
                 .edge(from(trendStage2).to(identifyPhase))
-                .edge(from(identifyPhase).to(loggerSink, 1)
-                                       .allToOne());
+                .edge(from(identifyPhase, 0).to(enrichWithNoiseInfo))
+                .edge(from(enrichWithNoiseInfo).to(averageNoise)
+                                               .allToOne())
+                .edge(from(averageNoise).to(loggerSink))
+
+                .edge(from(identifyPhase, 1).to(pollution))
+                .edge(from(enrichWithNoiseInfo).to(averageNoise)
+                                               .allToOne())
+                .edge(from(averageNoise).to(loggerSink));
 
 
         Job job = jet.newJob(dag);
         job.join();
 
+    }
+
+    private static SortedMap<Integer, Integer> getPhaseNoiseLookupTable(String phase, WakeTurbulanceCategory wtc) {
+        if ("TAKING_OFF".equals(phase)) {
+            if (HEAVY.equals(wtc)) {
+                return heavyWTCClimbingAltitudeToNoiseDb;
+            } else {
+                return mediumWTCClimbingAltitudeToNoiseDb;
+            }
+        } else if ("LANDING".equals(phase)) {
+            if (HEAVY.equals(wtc)) {
+                return heavyWTCDescendAltitudeToNoiseDb;
+            } else {
+                return mediumWTCDescendAltitudeToNoiseDb;
+            }
+        }
+        return Collections.emptySortedMap();
     }
 
 
--- src/main/java/com/hazelcast/jet/demo/FlightDataSource.java
+++ src/main/java/com/hazelcast/jet/demo/FlightDataSource.java
@@ -12,11 +12,18 @@ import com.hazelcast.jet.core.ProcessorMetaSupplier;
 import com.hazelcast.util.ExceptionUtil;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.net.ssl.HttpsURLConnection;
 
 import static com.hazelcast.jet.core.ProcessorMetaSupplier.dontParallelize;
+import static java.net.URLEncoder.encode;
 
 /**
  * date: 1/8/18
@@ -28,6 +35,8 @@ public class FlightDataSource extends AbstractProcessor {
     private final long intervalMillis;
     private Traverser<Aircraft> aircraftTraverser;
     private boolean lastTraverserExhausted = true;
+    private List<String> knownIds = new ArrayList<>(10000);
+    private int[] count;
 
     public FlightDataSource(String url, long intervalMillis) {
         try {
@@ -44,22 +53,31 @@ public class FlightDataSource extends AbstractProcessor {
 
 
     @Override public boolean complete() {
-        System.out.println("FlightDataSource.complete");
-        if (lastTraverserExhausted){
+        if (lastTraverserExhausted) {
             poll();
         }
         lastTraverserExhausted = emitFromTraverser(aircraftTraverser);
-        System.out.println("lastTraverserExhausted = " + lastTraverserExhausted);
         return false;
     }
 
     private void poll() {
-        System.out.println("FlightDataSource.poll");
         try {
+            String ids = knownIds.stream().collect(Collectors.joining("-"));
+            byte[] out = (encode("ICAOS", "UTF-8") + "=" + encode(ids, "UTF-8")).getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
             HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 
-            con.setRequestMethod("GET");
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
             con.addRequestProperty("User-Agent", "Mozilla / 5.0 (Windows NT 6.1; WOW64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 40.0.2214.91 Safari / 537.36");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            con.setRequestProperty("Content-Length", String.valueOf(length));
+
+            try (OutputStream os = con.getOutputStream()) {
+                os.write(out);
+            }
+
             con.getResponseCode();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -77,10 +95,10 @@ public class FlightDataSource extends AbstractProcessor {
             aircraftTraverser = Traversers.traverseStream(acList.values().stream().map(ac -> {
                         Aircraft aircraft = new Aircraft();
                         aircraft.fromJson(ac.asObject());
+                        knownIds.add(aircraft.getIcao());
                         return aircraft;
                     })
             );
-            System.out.println("aircraftTraverser = " + aircraftTraverser);
         } catch (Exception e) {
             throw ExceptionUtil.rethrow(e);
         }
